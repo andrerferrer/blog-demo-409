b3c2b5cb14f435337eadb789725257f5c4288909
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-29 10:46:34 -0300

    Finish the livecode. Sorry for the mess, cause we have too many files on this commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  # has_one_attached :banana
+  # has_one_attached :pdf
+  # has_one_attached :document
+  # This can be anything
+  has_many_attached :photos
 end
